339f4a87e0f258d64f1d53dd5d5be473f816a75b
Ciaran Hannigan <cehannig@us.ibm.com>
2015-05-11 21:24:38 -0400

    73960: 73960: build script to clean, build, rename library file and generate javadocs

--- move-aar-to-test-app.sh
+++ move-aar-to-test-app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 echo "moving app-debug.aar to test app as presence-insights-v1.1.aar"
-cp ./app/build/outputs/aar/app-debug.aar ../pi-clientsdk-android-test/app/libs/presence-insights-v1.1.aar
+
+cp ./app/build/outputs/aar/app-release.aar ../pi-clientsdk-android-test/app/libs/presence-insights-v1.1.aar
 echo "completed move"
