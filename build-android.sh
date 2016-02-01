b22c4edf2807dccd05a5558b038fb1f002da2b4a
CIARAN E. HANNIGAN <cehannig@us.ibm.com>
2016-02-01 14:22:40 -0500

    renamed library root folder to pi-sdk. added gradle plugins to import library to JCenter and MavenCentral.

--- build-android.sh
+++ build-android.sh
@@ -4,8 +4,6 @@ echo "running gradle clean"
 ./gradlew clean
 echo "running gradle build"
 ./gradlew build
-echo "generating javadocs"
-./gradlew generateReleaseJavadoc
 echo "changing file name from app-release to presence-insights.aar"
 cp ./app/build/outputs/aar/app-release.aar ./app/build/outputs/aar/presence-insights.aar
 
