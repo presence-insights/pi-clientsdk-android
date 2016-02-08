5a362d3ae958874dc1fbf09a02501543529430a2
CIARAN E. HANNIGAN <cehannig@us.ibm.com>
2016-02-08 12:09:54 -0500

    add ANDROID_ID as the new unique id for device registration. revamped PIDeviceInfo by restricting most of its values to being only settable in the constructors. Updates to the sensor and adapter were required for this.

--- move-aar-to-test-app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "moving app-debug.aar to test app as presence-insights-v1.1.aar"
-
-cp ./app/build/outputs/aar/app-debug.aar ../pi-clientsdk-android-test/app/libs/presence-insights-v1.1.aar
-echo "completed move"
