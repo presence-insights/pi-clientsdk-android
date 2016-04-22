37a4cb22e034a450931104fc87d2428dc2430d24
Laurent Cohen <laurentc@fr.ibm.com>
2016-04-22 10:51:20 +0200

    removed obsolete code; added checks on @updated and @created on geofence parsing; fixed issue with use of wrong device descriptor

--- pi-geofence/src/main/java/com/ibm/pi/geofence/GeofencingJSONUtils.java
+++ pi-geofence/src/main/java/com/ibm/pi/geofence/GeofencingJSONUtils.java
@@ -39,10 +39,6 @@ class GeofencingJSONUtils {
      * Date format used to convert dates from/to UTC format such as "2015-08-24T09:00:00-05:00".
      */
     static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-    /**
-     * Date format used to convert dates from/to UTC format such as "2015-08-24T09:00:00.123Z".
-     */
-    private static final String TMP_DESC = UUID.randomUUID().toString();
 
     /**
      * Parse a list of geofences.
@@ -87,13 +83,19 @@ class GeofencingJSONUtils {
     static PersistentGeofence parseGeofence(JSONObject feature) throws Exception {
         JSONObject props = feature.getJSONObject("properties");
         String code = props.has("code") ? props.getString("code") : (props.has("@code") ? props.getString("@code") : null);
-        JSONObject updatedJSON = props.getJSONObject("@updated");
-        long updated = updatedJSON.getLong("timestamp");
-        JSONObject createdJSON = props.getJSONObject("@created");
-        long created = createdJSON.getLong("timestamp");
-        String name = props.has("name") ? props.getString("name") : null;
-        String description = props.has("description") ? props.getString("description") : null;
-        double radius = props.has("radius") ? props.getDouble("radius") : -1d;
+        long updated = 0L;
+        JSONObject updatedJSON = props.optJSONObject("@updated");
+        if (updatedJSON != null) {
+            updated = updatedJSON.optLong("timestamp", 0L);
+        }
+        long created = 0L;
+        JSONObject createdJSON = props.optJSONObject("@created");
+        if (createdJSON != null) {
+            created = createdJSON.optLong("timestamp", 0L);
+        }
+        String name = props.optString("name", null);
+        String description = props.optString("description", null);
+        double radius = props.optDouble("radius", -1d);
         JSONObject geometry = feature.getJSONObject("geometry");
         JSONArray coord = geometry.getJSONArray("coordinates");
         double lng = coord.getDouble(0);
@@ -113,99 +115,6 @@ class GeofencingJSONUtils {
         geofence.setCreatedTimestamp(created);
         geofence.setUpdatedTimestamp(updated);
         return geofence;
-    }
-
-    /*
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ 10.1, 12.4 ]
-      },
-      "properties": {
-        "name": "France lab",
-        "description": "string",
-        "radius": 200
-      }
-    }
-    */
-    static JSONObject toJSONGeofence(PIGeofencingManager service, PersistentGeofence fence, boolean isUpdate) {
-        JSONObject json = new JSONObject();
-        try {
-            json.put("type", "Feature");
-            JSONObject geometry = new JSONObject();
-            json.put("geometry", geometry);
-            geometry.put("type", "Point");
-            JSONArray coord = new JSONArray();
-            geometry.put("coordinates", coord);
-            coord.put(fence.getLongitude());
-            coord.put(fence.getLatitude());
-            JSONObject properties = new JSONObject();
-            json.put("properties", properties);
-            properties.put("name", fence.getName());
-            properties.put("description", fence.getDescription() == null ? "" : fence.getDescription());
-            properties.put("radius", fence.getRadius());
-            if (isUpdate) {
-                JSONObject updated = new JSONObject();
-                properties.put("@updated", updated);
-                updated.put("by", service.httpService.getUsername());
-                updated.put("timestamp", fence.getUpdatedTimestamp());
-            }
-        } catch(JSONException e) {
-            log.error("exception generating json for geofence " + fence, e);
-        }
-        return json;
-    }
-
-    /*
-    {
-      "name": "string",
-      "registrationTypes": [ "Internal" ],
-      "description": "string",
-      "publicKey": "-----BEGIN PUBLIC KEY-----blabhblahblah=-----END PUBLIC KEY-----"
-    }
-    */
-    static JSONObject toJSONPostOrg(String name, String description, String publicKey) {
-        JSONObject json = new JSONObject();
-        try {
-            json.put("name", name).put("description", description).put("publicKey", publicKey);
-            JSONArray regTypes = new JSONArray();
-            json.put("registrationTypes", regTypes.put("Internal"));
-        } catch(JSONException e) {
-            log.error("exception generating json for org " + name, e);
-        }
-        return json;
-    }
-
-    /*
-    {
-        "name": "string",
-        "registrationTypes": ["Internal"],
-        "description": "string",
-        "publicKey": "-----BEGIN PUBLIC KEY-----blahblahblah=-----END PUBLIC KEY-----",
-        "@docType": "org",
-        "@updated": {
-            "by": "0f90fsy",
-            "timestamp": 1453976376453
-        },
-        "@created": {
-            "by": "0f90fsy",
-            "timestamp": 1453976376453
-        },
-        "@tenant": "xf504jy",
-        "@code": "ob6ppdp",
-        "@sites": "http://localhost:3000/pi-config/v2/tenants/xf504jy/orgs/ob6ppdp/sites",
-        "@devices": "http://localhost:3000/pi-config/v2/tenants/xf504jy/orgs/ob6ppdp/devices",
-        "@geofences": "http://localhost:3000/pi-config/v2/tenants/xf504jy/orgs/ob6ppdp/geofences"
-    }
-    */
-    static String parsePostOrgResponse(JSONObject json) {
-        try {
-        return json.getString("@code");
-        } catch(Exception e) {
-            log.error("exception parsing json for org: ", e);
-        }
-        return null;
     }
 
     /*
@@ -242,7 +151,7 @@ class GeofencingJSONUtils {
     static JSONObject toJSONGeofenceEvent(PersistentGeofence fence, String deviceID, String date, PIGeofenceEvent.Type type) {
         JSONObject json = new JSONObject();
         try {
-            json.put("descriptor", TMP_DESC);
+            json.put("descriptor", deviceID);
             //json.put("descriptor", deviceID);
             json.put("detectedTime", date);
             JSONObject data = new JSONObject();
@@ -254,27 +163,6 @@ class GeofencingJSONUtils {
             log.error("exception generating json for geofence list", e);
         }
         return json;
-    }
-
-    static PersistentGeofence updateGeofenceTimestampsFromJSON(PersistentGeofence geofence, JSONObject json) {
-        try {
-            JSONObject properties = json.getJSONObject("properties");
-            JSONObject created = properties.getJSONObject("@created");
-            long createdTImestamp = created.getLong("timestamp");
-            geofence.setCreatedTimestamp(createdTImestamp);
-            long updatedTimestamp = 0L;
-            if (properties.has("@updated")) {
-                JSONObject updated = properties.getJSONObject("@updated");
-                updatedTimestamp = updated.getLong("timestamp");
-            }
-            if (updatedTimestamp <= 0d) {
-                updatedTimestamp = createdTImestamp;
-            }
-            geofence.setUpdatedTimestamp(updatedTimestamp);
-        } catch(JSONException e) {
-            log.error("exception geofence timestamps for " + json, e);
-        }
-        return geofence;
     }
 
     static String formatDate(Date date) {
